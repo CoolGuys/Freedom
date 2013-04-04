1e1fd7a03180ecf8bcbfe7bf50bbc7b2e3427be7
ush <ush@ubuntu>
2013-04-04 22:08:18 +0400

    Промежуточная версия Loader

--- src/com/freedom/core/Loader.java
+++ src/com/freedom/core/Loader.java
@@ -1,10 +1,10 @@
 package com.freedom.core;
-
+//package com.freedom.gameObjects;
 import java.io.*;
 import java.util.*;
 import java.util.logging.Logger;
 
-import com.freedom.gameObjects.Stuff;
+import com.freedom.gameObjects.*;
 /**
  * Данный класс предназначен для чтения уровней из файла
  * В данный момент он не закончен, потому что требуется возможность добавлять объекты в массив Tiles
@@ -13,37 +13,47 @@ import com.freedom.gameObjects.Stuff;
  */
 public class Loader {
 	public static void test(int Number) throws FileNotFoundException {
+		System.out.print("loader start ");
 		Scanner sc;
 		sc = new Scanner(new File("Level1.lvl"));
-		while (sc.next().equals("Lavel")) {
+		String StrDump = sc.next();
+		System.out.println(StrDump);
+		while (StrDump.equals("Level")) {
 			int LevelNum = sc.nextInt();
-			String StrDump = sc.next();
+			StrDump = sc.next();
+			System.out.println(StrDump);
 			while (!StrDump.equals("/level")) {
 				if (Number == LevelNum) {
-					Stuff newstuff;
+					Object newstuff;
 					try {
-						newstuff = (Stuff) Class.forName(StrDump).newInstance();
+						Class cla=Class.forName(StrDump);
+						newstuff = cla.newInstance();
+						//newstuff.
+						//System.out.println(newstuff.getClass().getName());
 						newstuff.ReadLvlFile(sc);
 					} catch (InstantiationException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-						logger.severe("Ошибка при создании объекта типа stuff! --"+e.getMessage());
+						logger.severe("Error creating stuff! --"+e.getMessage());
 						break;
 					} catch (IllegalAccessException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-						logger.severe("Ошибка при создании объекта типа stuff! --"+e.getMessage());
+						logger.severe("Error creating stuff! --"+e.getMessage());
 						break;
 					} catch (ClassNotFoundException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-						logger.severe("Ошибка при создании объекта типа stuff! --"+e.getMessage());
+						logger.severe("Error creating stuff! --"+e.getMessage());
 						break;
 					}					
 				}
 			}
+			
 		}
+		System.out.println("end");
+		
 	}
-
+	
 	private static Logger logger = Logger.getLogger("Loader");
 }
