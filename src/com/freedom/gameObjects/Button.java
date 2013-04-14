622757b20acc5ea8c3b4b3b53b24ce218c5c619c
ivan <ivtakm@gmail.com>
2013-04-14 15:59:28 +0400

    порция

--- src/com/freedom/gameObjects/Button.java
+++ src/com/freedom/gameObjects/Button.java
@@ -2,6 +2,7 @@ package com.freedom.gameObjects;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 import javax.imageio.ImageIO;
 
@@ -9,12 +10,19 @@ public class Button extends Stuff{
 	
 	protected boolean ifPressed;
 	
+	ArrayList<Stuff> leadTo = new ArrayList<Stuff>();
 	
 	//при инициализации не нажата, что нормально
-	public Button(int x, int y){
+	//принимаются заявки на изменение входных данных)
+	public Button(int x, int y, Stuff[] objects){
 		super(false,true);
 		super.x = x;
 		super.y = y;
+		
+		for(int i = 0; i < objects.length; i++){
+			this.leadTo.add(objects[i]);
+		}
+		
 		try {
 			texture = ImageIO.read(new File("Resource/Textures/Button.png"));
 		} catch (IOException e) {
@@ -22,6 +30,33 @@ public class Button extends Stuff{
 			e.printStackTrace();
 		}
 		ifPressed = false;
+	}
+	
+	//конструктор для кнопки, которая будет ссылаться на 1 элем
+	//, неудобно ведь толкать в массив
+	public Button(int x, int y, Stuff thing){
+		super(false,true);
+		super.x = x;
+		super.y = y;
+		
+			this.leadTo.add(thing);
+		
+		try {
+			texture = ImageIO.read(new File("Resource/Textures/Button.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ifPressed = false;
+	}
+	
+	protected void touch(){
+		
+		for(int i = 0; i<this.leadTo.size()-1; i++){
+				Stuff buf = this.leadTo.get(i);
+				buf.buttonPressed();   ////!!!!!!!!!!!не фэйл ли?
+			}
+		this.ifPressed = !this.ifPressed;
 	}
 
 }
--- src/com/freedom/gameObjects/Door.java
+++ src/com/freedom/gameObjects/Door.java
@@ -4,38 +4,44 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Door extends Stuff{
-	
+public class Door extends Stuff {
+
 	protected boolean ifOpen;
-	private Button controller;
-	
-	
-	public Door(int x, int y, boolean ifOpen){
-		super(false,false);
+
+	public Door(int x, int y, boolean ifOpen) {
+		super(false, false);
 		super.x = x;
 		super.y = y;
 		this.ifOpen = ifOpen;
 		try {
-			texture = ImageIO.read(new File("Resource/Textures/DoorClosed.png"));
+			texture = ImageIO
+					.read(new File("Resource/Textures/DoorClosed.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void tryOpen(){
-		if (!controller.ifPressed){
-			return;
+
+	protected void buttonPressed() {
+		if (this.ifOpen) {
+			try {
+				texture = ImageIO.read(new File(
+						"Resource/Textures/DoorClosed.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.ifOpen = false;
+		} else {
+			try {
+				texture = ImageIO.read(new File(
+						"Resource/Textures/DoorOpened.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.ifOpen = true;
 		}
-		
-		ifOpen = true;
-		try {
-			texture = ImageIO.read(new File("Resource/Textures/DoorClosed.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return;
 	}
 
 }
