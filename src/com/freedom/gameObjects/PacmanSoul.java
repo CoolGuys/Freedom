8ebf77651f8598d331d98b01d4f497debf48b61a
Vdrhtc <vdrhtc@gmail.com>
2013-05-03 22:48:30 +0400

    PACKMEN PIRFARMENS EMPRUVEMENT1!1!!!1!

--- src/com/freedom/gameObjects/PacmanSoul.java
+++ src/com/freedom/gameObjects/PacmanSoul.java
@@ -48,6 +48,13 @@ public class PacmanSoul implements Runnable {
 
 	public void run() {
 		while (alive) {
+
+			try {
+				Thread.sleep(this.stepRate);
+			} catch (InterruptedException e) {
+				// TODO Автоматически созданный блок catch
+				
+			}
 			int x = body.getX();
 			int y = body.getY();
 			boolean b = (GameField.getInstance().getRobot() == null);
@@ -83,12 +90,6 @@ public class PacmanSoul implements Runnable {
 				this.destinationX = y;
 				this.destinationY = x;
 				this.alive=true;
-				try {
-					Thread.sleep(this.stepRate);
-				} catch (InterruptedException e) {
-					// TODO Автоматически созданный блок catch
-					
-				}
 				//System.out.println("Else");
 			}
 		}
